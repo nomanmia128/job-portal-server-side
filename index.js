6d3f94ec507cc9dbdbf2c918d053e966c281eec4
Noman <nomanmia128@gmail.com>
2024-12-20 21:53:00 +0600

    added job application apis

--- index.js
+++ index.js
@@ -34,7 +34,13 @@ async function run() {
         const jobApplcationCallection = client.db('jobPortal').collection('job-applications');
 
         app.get('/jobs', async (req, res) => {
-            const cursor = jobsCollection.find();
+            const email = req.query.email;
+            let query = {};
+            if(email){
+                query = { hr_email: email }
+            }
+
+            const cursor = jobsCollection.find(query);
             const result = await cursor.toArray();
             res.send(result);
         })
@@ -43,6 +49,12 @@ async function run() {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
             const result = await jobsCollection.findOne(query);
+            res.send(result);
+        })
+
+        app.post('/jobs', async(req,res) => {
+            const newJob = req.body;
+            const result = await jobsCollection.insertOne(newJob);
             res.send(result);
         })
 
